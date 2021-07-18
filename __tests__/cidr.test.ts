f846742e5efdcbd68b109e69b7b979fdcfb74b4e
azmizar <azmizar.ujang@outlook.com>
2021-07-18 00:38:04 -0400

    udpate: more unittest

--- __tests__/cidr.test.ts
+++ __tests__/cidr.test.ts
@@ -1,7 +1,19 @@
 /**
 * App imports
 */
-import {CIDRModule, OCTECTS} from '../src';
+import {CIDRModule, IPTUPPLE, OCTECTS} from '../src';
+
+const obj1: OCTECTS = {
+  first: 172,
+  second: 21,
+  third: 0,
+  fourth: 1,
+  add: (num: number): OCTECTS => {return {} as any},
+  substract: (num: number): OCTECTS => {return {} as any;},
+  toArray: (): IPTUPPLE => {return [172, 21, 0, 1];},
+  toString: (): string => {return '172.21.0.1';},
+  toBinary: (): string => {return '10101100000101010000000000000001';}
+};
 
 /**
  * CIDR calculator unittests
@@ -17,10 +29,10 @@ describe('CIDR Calculator Module', () => {
      * OCTECTS from string
      */
     test('OCTECTS from STRING', () => {
-      const res: OCTECTS = CIDRModule.parseOctects('172.21.0.1');
+      const res = CIDRModule.parseOctects('172.21.0.1');
 
       expect(res).toBeDefined();
-      //expect(res).toContain()
+      expect(obj1).toMatchObject(obj1);
     });
   });
 });
